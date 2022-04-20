8782579d98b6e69a16131cb9162bfa5180b2e0a7
eduardosi25 <eduardosi25@hotmail.com>
2022-04-20 14:35:04 -0500

    test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -111,6 +111,7 @@ define([
         payload['metaData'].isConfigured = true;
 
         console.log("payloadDespues-->",payload['arguments'].execute.inArguments);
+        console.log("payload0-->",payload['arguments'].execute.inArguments[0]);
         connection.trigger('updateActivity', payload);
         console.log("termino save")
     }
