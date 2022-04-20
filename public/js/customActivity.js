3836260cc300c1596e9544036218bc771f4af662
eduardosi25 <eduardosi25@hotmail.com>
2022-04-20 11:01:35 -0500

    test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -61,7 +61,7 @@ define([
 
         var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-        console.log("inArguments---->",inArguments);
+        console.log("inArguments---->",inArguments.Nombre);
 
         $.each(inArguments, function (index, inArgument) {
             $.each(inArgument, function (key, val) {
