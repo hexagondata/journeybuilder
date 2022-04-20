4c96e76e7f2dc30c1af7db76cfe8c5303ed2dc10
eduardosi25 <eduardosi25@hotmail.com>
2022-04-20 11:19:04 -0500

    test

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -91,7 +91,7 @@ define([
         // var postcardTextValue = $('#postcard-text').val();
         console.log("entro a save")
         console.log("payloadAntes--> con Peomocion",payload['arguments'].execute.inArguments[0].Promocion);
-        console.log("payloadAntes--> con Phone",payload['arguments'].execute.inArguments[1].Phone;
+        console.log("payloadAntes--> con Phone",payload['arguments'].execute.inArguments[1].Phone);
         console.log("payloadAntes--> con Nombre",payload['arguments'].execute.inArguments[2].Nombre);
         //payload['arguments'].execute.inArguments = [{
         //    "tokens": authTokens
